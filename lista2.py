bbfbc6fa806dfb72774d8d3d20513e165c2f03b0
Arthur Pereira <arthur.a.p@ufsc.br>
2026-03-16 14:22:56 -0300

    Three new exercises done

--- lista2.py
+++ lista2.py
@@ -1,41 +1,27 @@
-#1
-preco = float(input())
-parcelas = int(input())
-salario = float(input())
+#7
+ddd = int(input())
+match(ddd):
+    case 61:
+        print("Brasilia")
+    case 71:
+        print("Salvador")
+    case 11:
+        print("Sao Paulo")
+    case 21:
+        print("Rio de Janeiro")
+    case 32:
+        print("Juiz de Fora")
+    case 19:
+        print("Campinas")
+    case 27:
+        print("Vitoria")
+    case 31:
+        print("Belo Horizonte")
+    case _:
+        print("DDD nao cadastrado")
 
-vp = preco/parcelas
-if(salario*0.3 < vp):
-    print("RECUSADO")
-else:
-    print("APROVADO")
+#8
+        
+
+
     
-#2
-
-vp = int(input())
-opcao = input()
-
-match opcao:
-    case "a":
-        print(vp*0.9)
-    case "b":
-        print(vp*0.95)
-    case "c":
-        print(vp)
-    case "d":
-        print(vp*0.8)
-    
-#3
-peso = float(input())
-altura = float(input())
-imc = peso / (altura**2)
-
-if(imc > 40):
-    print("Obesidade mórbida")
-elif(imc > 30):
-    print("Obesidade")
-elif(imc > 25):
-    print("Sobrepeso")
-elif(imc > 18.5):
-    print("Peso ideal")
-else:
-    print("Abaixo do peso")
